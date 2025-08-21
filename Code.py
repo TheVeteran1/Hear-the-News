f1508a69e79d2194535859e8ab32c471324ea755
TheVeteran1 <206950159+TheVeteran1@users.noreply.github.com>
2025-08-22 00:43:45 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ import pyttsx3
 import json
 
 query=input("What type of news are you interested in?")
-url=f"https://newsapi.org/v2/everything?q={query}&from=&sortBy=publishedAt&apiKey=dc3ab12015dc44098d5928f620a8e923"
+url=f"https://newsapi.org/v2/everything?q={query}&from=&sortBy=publishedAt&apiKey=your_api_key"
 r=requests.get(url)
 news=json.loads(r.text)
 
@@ -16,5 +16,32 @@ def reading():
          print(article["description"])
          print("-----------------------------")
 
-
 reading()
+
+
+def speak():
+     engine.say(str(news))
+
+ask=input("Do you want to Hear News")
+
+if ask=="Yes" or "yes":
+     speak()
+elif ask=="No" or "no":
+     print("Okay , No problem ")
+else:
+     print("Thanks for the answer")
+     
+
+
+engine.runAndWait()
+
+     
+
+
+
+
+
+
+
+ 
+      
